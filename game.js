66c4949be96374a34abd3826cf9ba421197e93c8
⚜️ A̍ͭͩnLͭ̇̎̏̒oͩM̳͇i͎̞̝̪n̹̘̓ͣ̓͊̓ů͕̈s̙͉͎̫̈̌̇̐̂ ⚜️ <51442719+AnLoMinus@users.noreply.github.com>
2026-02-06 18:45:28 +0200

    Create game.js - Main game engine and canvas rendering

---
+++ game.js
@@ -0,0 +1,13 @@
+// Main game engine and canvas rendering
+
+class Game {
+    constructor() {
+        // Initialization code
+    }
+    start() {
+        // Start the game
+    }
+}
+
+const game = new Game();
+game.start();
